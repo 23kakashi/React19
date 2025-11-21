9b4f38b38a2abf87eb1b666117b8f9f9dff7e8c1
Tapish Sharma <tapish-sharma@pluralsight.com>
2025-11-21 15:58:10 +0530

    travel journal done

---
+++ travel-journal/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <header>
+      <img src="src/assets/globe.png" alt="globe image" />
+      <span>my travel journal.</span>
+    </header>
+  );
+}
---
+++ travel-journal/src/components/Journal.jsx
@@ -0,0 +1,33 @@
+import "./journal.css";
+
+export default function Journal(props) {
+  const { title, img, country, googleMapsLink, dates, text } = props;
+  return (
+    <>
+      <div className="journal">
+        <div>
+          <img
+            src={img.src}
+            alt={`${title} image`}
+            className="location-image"
+          />
+        </div>
+        <div className="details">
+          <div className="country">
+            <img
+              src="src/assets/marker.png"
+              alt="location marker"
+              className="marker"
+            />
+            <span>{country}</span>
+            <a href={googleMapsLink}>View on Google Maps</a>
+          </div>
+          <p className="title">{title}</p>
+          <p className="date">{dates}</p>
+          <p className="text">{text}</p>
+        </div>
+      </div>
+      <hr />
+    </>
+  );
+}
